8c93acfc571c704f3fe9f130b694c7e391d8dc24
craig3050 <craig3050@users.noreply.github.com>
2014-04-24 08:41:08 +0100

    Fourth attempt in Reddit changes

--- previous.py
+++ previous.py
@@ -8,22 +8,16 @@
 # Copyright:   (c) Craig Cuninghame 2014
 # Licence:     <your licence>
 #-------------------------------------------------------------------------------
+
 from random import randint
 
-# Step 2 - Call that many numbers out of our list
+
 def return_list(num_to_try,numbers_dictionary):
 
     first = randint(1,6)
     list_fig = [int(numbers_dictionary[first])]
     called = [first]
     x = 1
-
-    print first
-    print called
-    print list_fig
-    print numbers_dictionary
-
-
     while x < num_to_try:
             y = randint(1,6)
             if y not in called:
@@ -32,7 +26,7 @@ def return_list(num_to_try,numbers_dictionary):
                 x += 1
     return list_fig
 
-# Step 3 - function to generate random Add/Subtract/Multiply/Divide
+
 def operator():
     x = randint(1,4)
     if x == 1:
@@ -44,35 +38,37 @@ def operator():
     if x == 4:
         return "division"
 
-# Step 4 - Add / Subtract / divide / multiply and
+
 def takes_two(a,b,c):
     if c == "addition":
-        return a + b
+        return float(a + b)
     if c == "subtraction":
-        return a - b
+        return float(a - b)
     if c == "multiplication":
-        return a * b
+        return float(a * b)
     if c == "division":
-        return a / b
+        return float(a / b)
 
-#see if it matches target
+
 def check_target(list):
-    #1 op 2 op 3 op 4 op 5 op 6 or however many there are
     x = len(list)
     count = 1
     total = list[0]
-    final_list = [total]
+    final_list = [float(total)]
     while count < x:
         op = operator()
-        total = takes_two(total,list[count],op)
+        second = list[count]
+        total = takes_two(total,second,op)
         count += 1
         final_list.append(op)
-        final_list.append(total)
+        final_list.append(second)
+        final_list.append(" = ")
+        final_list.append(float(total))
     return (total, final_list)
 
 
 
-# Step 5 - if matches print finished & last file entry, if not retry with different combination
+
 def main():
 
     print "********************************"
@@ -88,13 +84,16 @@ def main():
     numbers_dictionary[4] = raw_input("Enter fourth number")
     numbers_dictionary[5] = raw_input("Enter fifth number")
     numbers_dictionary[6] = raw_input("Enter sixth number")
-    target = raw_input("Enter the Target Number")
-
+    target = int(raw_input("Enter the Target Number"))
     total = 0
-    while total != target:
+
+    while target != total:
         num_to_try = randint(1,6)
-        total = check_target(return_list(num_to_try,numbers_dictionary))
+        (total , final_list) = check_target(return_list(num_to_try,numbers_dictionary))
         print total
+
+    print total
+    print final_list
 
 if __name__ == '__main__':
     main()
