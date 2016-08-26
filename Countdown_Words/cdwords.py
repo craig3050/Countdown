9e5841194bec347a2ad5dfa434711d329156bcfb
Craig Cuninghame <craig3050@gmail.com>
2016-08-26 20:48:22 +0100

    Shortens word length, much faster, presents matches in reverse order.
    Need to fix constant new line \n, and also printing the number from the
    dictionary too.

--- Countdown_Words/cdwords.py
+++ Countdown_Words/cdwords.py
@@ -4,18 +4,24 @@ import os
 dictionary = open('Dict.txt', 'r')     
 print ("Welcome to Craig's Countdown Words solving tool!!")
 cdwords = input("Enter all the letters as one big word")
+word_length = len(cdwords) + 1
 
 word_list = {}
 
 for words in dictionary:
-    letter_count = 0
-    for letters in cdwords:
-        if letters in words:
-            letter_count += 1
-    word_list[words] = letter_count
-    print (words + " " + str(letter_count))
+    if len(words) <= word_length:
+        letter_count = 0
+        for letters in cdwords:
+            if letters in words:
+                letter_count += 1
+        word_list[words] = letter_count
+        ##print (words + " " + str(letter_count))
     
+word_list = sorted(word_list, key=word_list.__getitem__, reverse = True)
 print (word_list)
+
+
+
 
    
 
